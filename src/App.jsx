1bea89f7314f6d6e7f8d1e9adbeca4546fbcf0dd
Barath <barathmurugesan6@gmail.com>
2024-04-06 19:26:52 +0530

    install react js and tailwindcss and modify the first template

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import About from './pages/About';
+import Profile from './pages/Profile';
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl  font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/sign-in" element={<Signin />}/>
+      <Route path="/sign-up" element={<Signup />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/profile" element={<Profile />}/>
+      
+      </Routes>
+      </BrowserRouter>
+
   ) 
 }
  
